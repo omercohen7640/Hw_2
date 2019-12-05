4a9eac9cd9773cb37b298d873d1abe7858731ad7
omercohen7640 <45284523+omercohen7640@users.noreply.github.com>
2019-12-05 18:43:02 +0200

    post debug runway

--- runway.c
+++ runway.c
@@ -2,26 +2,26 @@
 // struct for a node in the list
 typedef struct Node_flight_t {
 	Pflight Pdata; // pointer for the node data (flight)
-	PNode_flight Pnext_Node;
+	Pflight_node Pnext_Node;
 } flight_node;
 typedef struct run_way_t {
 	int runway_id;
-	ftype_t flight_type;
+	FlightType flight_type;
 	Pflight_node head;
 }Run_way;
-Prun_way createRunway(int id, ftype_t type)
+Prun_way createRunway(int id, FlightType type)
 {
 	Prun_way new_runway;
-	if (id < 1 || id > MAX_ID || (type != 0 && type != 1)
+	if (id < 1 || id > MAX_ID || (type != 0 && type != 1))
 	{
-		return null;
+		return NULL;
 	}
-	new_runway = (Prun_way)malloc(sizeof(run_way));
-	if (new_runway != null)
+	new_runway = (Prun_way)malloc(sizeof(Run_way));
+	if (new_runway != NULL)
 	{
 		new_runway->runway_id = id;
-		new_runway->flight_type = type;
-		new_runway->head = null;
+		new_runway->FlightType = type;
+		new_runway->head = NULL;
 	}
-	return Prun_way;	
+	return new_runway;	
 }
--- runway.h
+++ runway.h
@@ -2,20 +2,19 @@
 #define _RUNWAY_H_
 #include "ex2.h"
 #include "flight.h"
-typedef struct run_way* Prun_way;
+typedef struct run_way_t Run_way;
+typedef struct Run_way* Prun_way;
 typedef struct Node_flight_t* Pflight_node;
-typedef enum { FALSE, TRUE } BOOL;
-typedef enum { FAILURE, SUCCESS } ATTEMPT; //type for failure or succes
 //constructor
-Prun_way createRunway(int id, ftype_t type);
+Prun_way createRunway(int id, FlightType type);
 //distructor
 BOOL destroyRunway(Prun_way p_runway);
 //other
 BOOL isFlightExists(Prun_way p_runway, int id); // checks if flight with id exists on a run way
 int getFlightNum(Prun_way p_runway); //return the number of flights in a runway
 int getEmergency(Prun_way p_runway); //return the number of emergency flights
-ATTEMPT addFlight(Prun_way p_runway, Pflight p_flight); //add a flight to the run way
-ATTEMPT removeFlight(Prun_way p_runway, int id); //delete a flight from the runway by id
-ATTEMPT depart(Prun_way p_runway); // delete the first flight from the runway
+Result addFlight(Prun_way p_runway, Pflight p_flight); //add a flight to the run way
+Result removeFlight(Prun_way p_runway, int id); //delete a flight from the runway by id
+Result depart(Prun_way p_runway); // delete the first flight from the runway
 void printRunway(Prun_way p_runway); //print the list of flights in the runway
 #endif _RUNWAY_H_
